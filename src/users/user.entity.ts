241de87d4350de3091e0dc02d094323042f3ca9e
arden <ardenhilario@gmail.com>
2023-06-06 20:06:20 +0800

    Create User dto

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,7 +1,26 @@
-import { ApiProperty } from '@nestjs/swagger'
+import { ApiProperty, PartialType, PickType } from '@nestjs/swagger'
 import { User as UserModel } from '@prisma/client'
 
+const NAME = {
+  MIN_LENGTH: 1,
+  MAX_LENGTH: 50,
+  PATTERN: /^(?![ .-])(?!.*[ ]$)(?!.*?[.-]{2})[a-zA-Z .-]+$/g
+}
+const USERNAME = {
+  MIN_LENGTH: 6,
+  MAX_LENGTH: 25,
+  PATTERN: /^(?![0-9])(?![_.])(?!.*[_.]$)(?!.*?[_.]{2})[a-zA-Z0-9_.]+$/g
+}
+const PASSWORD = { MIN_LENGTH: 8 }
+Object.freeze(NAME)
+Object.freeze(USERNAME)
+Object.freeze(PASSWORD)
+
 export class UserEntity implements UserModel {
+  constructor(partial: Partial<UserEntity | null>) {
+    Object.assign(this, partial)
+  }
+
   @ApiProperty({
     example: 1,
     readOnly: true,
@@ -32,5 +51,32 @@ export class UserEntity implements UserModel {
     required: false
   })
   updatedAt!: Date
+}
+
+export class CreateUserDto extends PickType(UserEntity, ['name', 'username', 'password'] as const) {
+  @ApiProperty({
+    minLength: NAME.MIN_LENGTH,
+    maxLength: NAME.MAX_LENGTH,
+    pattern: NAME.PATTERN.toString()
+  })
+  name!: string
+
+  @ApiProperty({
+    minLength: USERNAME.MIN_LENGTH,
+    maxLength: USERNAME.MAX_LENGTH,
+    pattern: USERNAME.PATTERN.toString()
+  })
+  username!: string
+
+  @ApiProperty({ minLength: PASSWORD.MIN_LENGTH })
+  password!: string
+}
+
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  name?: string
+
+  username?: string
+
+  password?: string
 }
 
